0ff8014c9a782546ad0741de2edecd3916cc9fc1
elee2 <eunho.lee@agoda.com>
2024-01-17 23:21:23 +0700

    add dfs

--- src/main/java/org/example/leetcode/graph/CourseScheduleII.java
+++ src/main/java/org/example/leetcode/graph/CourseScheduleII.java
@@ -26,44 +26,108 @@ public class CourseScheduleII {
  * So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
  *
  */
-public int[] findOrder(int numCourses, int[][] prerequisites) {
-    List<Integer>[] adjList = new ArrayList[numCourses];
-    for(int i = 0; i < numCourses; i++) adjList[i] = new ArrayList<>();
 
-    int[] indegrees = new int[numCourses];
-    for(int[] pre : prerequisites) {
-        int from = pre[0];
-        int to = pre[1];
-        adjList[from].add(to);
-        indegrees[to]++;
-    }
+    /**
+     * Using Queue and Topological Sort
+     * TC: O(V+E) - V is the number of vertices and E is the number of edges
+     * SC: O(V+E)*
+     */
+    public int[] findOrderTopologicalSort(int numCourses, int[][] prerequisites) {
+        List<Integer>[] adjList = new ArrayList[numCourses];
+        for(int i = 0; i < numCourses; i++) adjList[i] = new ArrayList<>();
 
-    Queue<Integer> q = new LinkedList<>();
-    for(int i = 0; i < indegrees.length; i++) {
-        if(indegrees[i] == 0) q.add(i);
-    }
-
-    int[] ans = new int[numCourses];
-    Set<Integer> visited = new HashSet<>();
-    int pos = 0;
-    while(!q.isEmpty()) {
-        int curr = q.remove();
-        ans[pos++] = curr;
-        visited.add(curr);
-        for(int next : adjList[curr]) {
-            indegrees[next]--;
-            if(indegrees[next] == 0) q.add(next);
+        int[] indegrees = new int[numCourses];
+        for(int[] pre : prerequisites) {
+            int from = pre[0];
+            int to = pre[1];
+            adjList[from].add(to);
+            indegrees[to]++;
         }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < indegrees.length; i++) {
+            if(indegrees[i] == 0) q.add(i);
+        }
+
+        int[] ans = new int[numCourses];
+        Set<Integer> visited = new HashSet<>();
+        int pos = 0;
+        while(!q.isEmpty()) {
+            int curr = q.remove();
+            ans[pos++] = curr;
+            visited.add(curr);
+            for(int next : adjList[curr]) {
+                indegrees[next]--;
+                if(indegrees[next] == 0) q.add(next);
+            }
+        }
+
+        for(int i = 0; i < numCourses/2; i++) {
+            int tmp = ans[i];
+            ans[i] = ans[numCourses - 1 - i];
+            ans[numCourses - 1 - i] = tmp;
+        }
+
+        return visited.size() == numCourses ? ans : new int[]{};
     }
 
-    for(int i = 0; i < numCourses/2; i++) {
-        int tmp = ans[i];
-        ans[i] = ans[numCourses - 1 - i];
-        ans[numCourses - 1 - i] = tmp;
+    private static int WHITE = 1;
+    private static int GRAY = 2;
+    private static int BLACK = 3;
+
+    boolean foundCycle;
+    Map<Integer, Integer> colorMap;
+    Map<Integer, List<Integer>> adjList;
+    List<Integer> topologicalOrder;
+
+
+    public int[] findOrderDfs(int numCourses, int[][] prerequisites) {
+        this.foundCycle = false;
+        this.colorMap = new HashMap<>();
+        this.adjList = new HashMap<>();
+        this.topologicalOrder = new ArrayList<>();
+
+        for(int i = 0; i < numCourses; i++) this.colorMap.put(i, WHITE);
+
+        for(int[] pre : prerequisites) {
+            int from = pre[1];
+            int to = pre[0];
+            if(!adjList.containsKey(from)) adjList.put(from, new ArrayList<>());
+            adjList.get(from).add(to);
+        }
+
+        for(int i = 0; i < numCourses; i++) {
+            if(this.colorMap.get(i) == WHITE) dfs(i);
+        }
+
+        if(this.foundCycle) return new int[]{};
+
+        int[] ans = new int[numCourses];
+        for(int i = 0; i < numCourses; i++) {
+            ans[i] = this.topologicalOrder.get(numCourses - i - 1);
+        }
+        return ans;
     }
 
-    return visited.size() == numCourses ? ans : new int[]{};
-}
+    private void dfs(int node) {
+        if(this.foundCycle) return;
+
+        // start the recursion
+        this.colorMap.put(node, GRAY);
+
+        for(int next : this.adjList.getOrDefault(node, new ArrayList<Integer>())) {
+            if(this.colorMap.get(next) == WHITE) {
+                dfs(next);
+            }
+            else if(this.colorMap.get(next) == GRAY) {
+                // if we encounter the GRAY, it detects it has cycle
+                this.foundCycle = true;
+            }
+        }
+
+        this.colorMap.put(node, BLACK);
+        this.topologicalOrder.add(node);
+    }
 
 
 }
