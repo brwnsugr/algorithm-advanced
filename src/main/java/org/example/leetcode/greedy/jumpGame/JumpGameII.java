3877163c001e21377e5b4ce855f1c0cc96b27b61
brwnsugr <dangelove.n@gmail.com>
2024-01-18 23:16:50 +0700

    greedy jumpGameII

---
+++ src/main/java/org/example/leetcode/greedy/jumpGame/JumpGameII.java
@@ -0,0 +1,23 @@
+package org.example.leetcode.greedy.jumpGame;
+
+/**
+ * LeetCode #45. Jump Game II
+ *
+ */
+public class JumpGameII {
+    public int jump(int[] nums) {
+        if(nums.length == 1) return 0;
+        int currEnd = 0;
+        int currFar = 0;
+        int ans = 0;
+        for(int i = 0; i < nums.length; i++) {
+            currFar = Math.max(currFar, nums[i] + i);
+            if(i == currEnd) {
+                ans++;
+                currEnd = currFar;
+                if(currEnd >= nums.length - 1) return ans;
+            }
+        }
+        return ans;
+    }
+}
