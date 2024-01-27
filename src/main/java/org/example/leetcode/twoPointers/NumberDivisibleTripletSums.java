5deb303f2da24fea4b310d228f5c0b0f194fa714
brwnsugr <dangelove.n@gmail.com>
2024-01-27 22:30:53 +0700

    number of divisible triplet sums

---
+++ src/main/java/org/example/leetcode/twoPointers/NumberDivisibleTripletSums.java
@@ -0,0 +1,34 @@
+package org.example.leetcode.twoPointers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NumberDivisibleTripletSums {
+    public int divisibleTripletCount(int[] nums, int d) {
+        Map<Integer, List<int[]>> m = new HashMap<>();
+
+        for(int i = 0; i < nums.length - 1; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                int key = (nums[i]+nums[j])% d;
+                if(!m.containsKey(key)) m.put(key, new ArrayList<>());
+                m.get(key).add(new int[]{i, j});
+            }
+        }
+
+        int answer = 0;
+
+        for(int l = 0; l < nums.length; l++) {
+            int key = (d - nums[l] % d) % d;
+            int tmpCount = 0;
+            for(int[] remainArr : m.getOrDefault(key, new ArrayList<>())) {
+                int remainLeftIdx = remainArr[0];
+                if(l < remainLeftIdx) tmpCount++;
+            }
+            answer += tmpCount;
+        }
+
+        return answer;
+    }
+}
